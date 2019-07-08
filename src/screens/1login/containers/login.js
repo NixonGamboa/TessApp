8ad43f0caa2bac2db82f2909d83e7fb624c392b2
Nixon Gamboa <nrgamboa97@gmail.com>
2019-07-08 18:07:42 -0500

    Eliminados inputs del login

--- src/screens/1login/containers/login.js
+++ src/screens/1login/containers/login.js
@@ -4,9 +4,6 @@ import {GoogleSigninButton } from 'react-native-google-signin';
 
 import Logo from '../components/logo';
 import Layout from '../components/layout';
-import NameInput from '../components/name-input';
-import CelInput from '../components/cel-input';
-import OkBtn from '../components/ok-btn';
 import moment from "moment";
 
 import googleLogin from '../../../firebase/google-signin';
@@ -24,43 +21,6 @@ class Login extends Component {
 	componentDidMount(){
 	
 	}
-	onChangeName = (text) => {
-		this.props.dispatch({
-			type:'SET_USERNAME',
-			payload:{
-				userName:text,
-			}
-		})
-	}
-	onChangeCel = (text) => {
-		this.props.dispatch({
-			type:'SET_USERCEL',
-			payload:{
-				userCel:text,
-			}
-		})
-	}
-	handlePress = ()=> {
-		if (this.props.name === '' ){
-			alert('Por favor ingrese un nombre valido')
-		}
-		else if (this.props.cel.length !== 10){
-			alert('Por favor ingrese un numero de celular valido')
-		}
-		else{
-			console.log('valido')
-			//AQUI ES DONDE SE PUEDE HACER UN LLAMADO FETCH AL API PARA VALIDAR
-			this.props.dispatch({
-				type:'SET_ACTIVEUSER',
-				payload:{
-					userActive:true,
-				}
-			})
-			this.props.navigation.navigate('Home')
-			console.log('Home')
-		}
-
-	}
 	signIn = () => {
 		console.log('iniciando signIn')
 		var us =  googleLogin()
@@ -74,14 +34,7 @@ class Login extends Component {
 			<Layout>
 				<Logo/>
 				<Layout> 
-					<NameInput
-						onChangeName= {this.onChangeName}
-						name={this.props.name} />
-					<CelInput
-						onChangeCel={this.onChangeCel} 
-						cel={this.props.cel} />
-					<OkBtn 
-						onPress={this.handlePress} />
+					
 					<GoogleSigninButton
 					    style={{ width: 240, height: 50 }}
 					    size={GoogleSigninButton.Size.Wide}
